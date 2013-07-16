2020ea16a472f903ee3e418573b7e2d5f5de3e4d
John Horstman <github@johnhorstman.com>
2013-07-16 17:48:45 -0500

    Another modification for fixing images in IE

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+// $(document).on('pop-initialized', function(){
+//   $('.asset-type-imagegroup').live('initialize', function(){
+//     $(this).find('.slide').each(function(){
+//       var $link = $(this).find('a');
+//       $link.find('img').insertBefore($link);
+//       $link.addClass('image-overlay').css({'line-height': $(this).height() + 'px'});
+//     });
+//   });
+// });
+
 $(document).on('pop-initialized', function(){
   $('.asset-type-imagegroup').live('initialize', function(){
     $(this).find('.slide').each(function(){
-      var $link = $(this).find('a');
-      $link.find('img').insertBefore($link);
+      var $el = $(this).find('a');
+      $link = $el.clone();
+      $link.find('img').remove();
       $link.addClass('image-overlay').css({'line-height': $(this).height() + 'px'});
+      $link.insertAfter($el);
     });
   });
 });
